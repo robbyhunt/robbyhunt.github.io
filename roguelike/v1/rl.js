bd5bb1fb7a4832527f1b0bf62e76d216126ea54f
Robby Hunt <robbyshunt@gmail.com>
2018-01-18 14:17:27 -0800

    player and mobs

--- roguelike/v1/rl.js
+++ roguelike/v1/rl.js
@@ -8,6 +8,14 @@ var COLS = 50;
 // number of actors per level (includes player)
 var ACTORS = 10;
 
+// a list of all actors; 0 is the player
+var player;
+var actorList;
+var livingEnemies;
+
+// points to each actor in it's position for quick searching
+var actorMap;
+
 // the structure of the map
 var map;
 
@@ -34,7 +42,12 @@ function create() {
 		for (var x=0;x<COLS;x++)
 			newRow.push( initCell('', x, y) );
 	}
+
+
+	initActors();
+
 	drawMap();
+	drawActors();
 }
 
 function initCell(chr, x, y) {
@@ -62,6 +75,44 @@ function drawMap() {
 	for (var y = 0; y < ROWS; y++)
 		for (var x = 0; x < COLS; x++)
 			asciidisplay[y][x].content = map[y][x];
+}
+
+function randomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function initActors() {
+	// create actors at random loc
+	actorList = [];
+	actorMap = {};
+	for (var e = 0; e < ACTORS; e++) {
+		// create new actor
+		var actor = { 
+            x: 0,
+            y: 0,
+            hp: e == 0 ? 3 : 1 
+        };
+		do {
+			// pick a random position that is both a floor and not occupied
+			actor.y = randomInt(ROWS);
+			actor.x = randomInt(COLS);
+		} while (map[actor.y][actor.x] == '#' || actorMap[actor.y + "_" + actor.x] != null);
+
+		/// add references to the actor in the actors list & map
+		actorMap[actor.y + "_" + actor.x] = actor;
+		actorList.push(actor);
+	}
+
+	// the player is the first actor in the list
+	player = actorList[0];
+	livingEnemies = ACTORS - 1;
+}
+
+function drawActors() {
+	for (var a in actorList) {
+		if (actorList[a].hp > 0)
+			asciidisplay[actorList[a].y][actorList[a].x].content = a == 0 ? '' + player.hp : 'e';
+	}
 }
 
 function onKeyUp(event) {
